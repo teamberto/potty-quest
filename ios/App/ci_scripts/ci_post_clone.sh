7387db4a4d8f176d4104de095763fa2ee05796d7
teamberto <albertojohnson1@gmail.com>
2026-07-08 21:51:40 -0500

    Update CI post-clone script for Node and pods

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-   set -e
+set -e
 
-   cd "$CI_PRIMARY_REPOSITORY_PATH"
-   npm install
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+brew install node@18
+brew link node@18 --force
 
-   cd ios/App
-   pod install
+cd "$CI_PRIMARY_REPOSITORY_PATH"
+npm install
+
+cd ios/App
+pod install
